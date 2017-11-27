9201e4d3a6509748cd86e22fe2ee710edc719d79
Roger <rogeliomer_2@hotmail.com>
2017-11-26 23:18:04 -0600

    fixed hub route

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,7 +1,7 @@
 const requireLogin = require('../middleware/requireLogin');
 
 module.exports = (app) => {
-    app.get('/hub', requireLogin, (req, res) => {
+    /*app.get('/hub', requireLogin, (req, res) => {
         res.send('Success you are on Dashboard')
-    });
+    });*/
 };
